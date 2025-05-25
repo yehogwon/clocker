84ba1938fe1d396b44b4c8ee2906d853e70b2638
Yeho Gwon <yeho.gwon@postech.ac.kr>
2025-05-25 13:44:59 +0900

    implement basic features

---
+++ clocker/clocker.py
@@ -0,0 +1,32 @@
+from functools import wraps
+from time import time
+
+def _millis() -> float: 
+    """
+    Returns the current time in milliseconds.
+    """
+    return round(time() * 1000)
+
+class clock: 
+    def __enter__(self): 
+        self.start = _millis()
+        return self
+
+    def __exit__(self, *args):
+        pass
+
+    def __call__(self, func): 
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            _start = _millis()
+            result = func(*args, **kwargs)
+            _end = _millis()
+            _func_name = func.__name__
+            print(f'Function \'{_func_name}\' executed in {_end - _start:.4f} ms')
+            return result
+        return wrapper
+    
+    @property
+    def elapsed(self): 
+        self.end = _millis()
+        return self.end - self.start
